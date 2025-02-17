b23a7b6376b9ad84c8b54dd2b868fba987a18106
sharifabee <sharifabee.alur@gmail.com>
2025-02-17 15:57:50 +0530

    nav bar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -13,10 +13,10 @@ export default function NavBar() {
     </button>
     <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
       <div className="navbar-nav">
-        <a className="nav-link active" aria-current="page" href="#">AddStudent</a>
-        <a className="nav-link" href="#">DisplayAll</a>
-        <a className="nav-link" href="#">Search</a>
-        <a className="nav-link" href="#">Delete</a>
+        <a className="nav-link active" aria-current="page" href="/add">AddStudent</a>
+        <a className="nav-link" href="/">DisplayAll</a>
+        <a className="nav-link" href="/search">Search</a>
+        <a className="nav-link" href="/delete">Delete</a>
        
        </div>
     </div>
