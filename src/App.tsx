22cd9dd64a57dead348da1d80956ccf82e53ac1a
sharifabee <sharifabee.alur@gmail.com>
2025-02-17 15:53:23 +0530

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,25 @@
 import React from 'react'
 import AddStudent from './components/AddStudent'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import DisplayAll from './components/DisplayAll'
+import SearchStudent from './components/SearchStudent'
+import DeleteStudent from './components/DeleteStudent'
 
 export default function App() {
   return (
     <div>
 
-<AddStudent />
+<BrowserRouter>
+
+<Routes>
+<Route path='/' element={<DisplayAll />} />
+<Route path='/add' element={<AddStudent />} />
+<Route path='/search' element={<SearchStudent />} />
+<Route path='/delete' element={<DeleteStudent />} />
+
+</Routes>
+
+</BrowserRouter>
 
 
     </div>
